a3b9e9a545f211b271f1aae2340d319fd5b5d76e
Nathan <naccaratonathan@gmail.com>
2023-03-28 09:33:30 -0400

    push

--- src/main/java/practice_exercise_3_9/SavingsAccount.java
+++ src/main/java/practice_exercise_3_9/SavingsAccount.java
@@ -1,10 +1,17 @@
 package practice_exercise_3_9;
 
 public class SavingsAccount {
-
+    /**
+     * Creates a instance variable for balance
+     */
     private double bal;
+    /**
+     * creates a instance variable for interest rate
+     */
     private double intRate;
-
+    /**
+     *
+     */
     public SavingsAccount(double startingBalance, double intRate) {
         bal = startingBalance;
         this.intRate = intRate;

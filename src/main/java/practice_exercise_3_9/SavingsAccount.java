74decad1dcc96331d1d0f06c31c4e8c2e2ec28f1
nacc2221 <naccaratonathan@gmail.com>
2023-03-28 12:07:23 -0400

    push

--- src/main/java/practice_exercise_3_9/SavingsAccount.java
+++ src/main/java/practice_exercise_3_9/SavingsAccount.java
@@ -2,25 +2,32 @@ package practice_exercise_3_9;
 
 public class SavingsAccount {
     /**
-     * Creates a instance variable for balance
+     * Creates an instance variable for balance.
      */
     private double bal;
     /**
-     * creates a instance variable for interest rate
+     * creates an instance variable for interest rate.
      */
     private double intRate;
     /**
-     *
+     Constructs a bank account with a given balance.
+     @param startingBalance is the initial balance given in the savings account.
+     @param intRate is the interest rate.
      */
     public SavingsAccount(double startingBalance, double intRate) {
         bal = startingBalance;
         this.intRate = intRate;
     }
-
+    /**
+     Gets the current balance of the bank account.
+     @return returns the balance in the savings account.
+     */
     public double getBal() {
         return bal;
     }
-
+    /**
+     Adds the interest rate to the account balance.
+     */
     public void addInterest() {
         double interest = bal * intRate / 100;
         bal += interest;

51e0adfeb400838533013962fe83015691f207a5
Nathan <naccaratonathan@gmail.com>
2023-03-28 09:11:16 -0400

    push

--- src/main/java/practice_exercise_3_9/SavingsAccount.java
+++ src/main/java/practice_exercise_3_9/SavingsAccount.java
@@ -2,4 +2,21 @@ package practice_exercise_3_9;
 
 public class SavingsAccount {
 
+    private double bal;
+    private double intRate;
+
+    public SavingsAccount(double startingBalance, double intRate) {
+        bal = startingBalance;
+        this.intRate = intRate;
+    }
+
+    public double getBalance() {
+        return bal;
+    }
+
+    public void addInterest() {
+        double interest = bal * intRate / 100;
+        bal += interest;
+    }
+
 }
--- src/main/java/practice_exercise_3_9/SavingsAccountTester.java
+++ src/main/java/practice_exercise_3_9/SavingsAccountTester.java
@@ -1,5 +1,11 @@
 package practice_exercise_3_9;
 
 public class SavingsAccountTester {
-    
+    public static void main(String[] args) {
+        SavingsAccount account = new SavingsAccount(1000, 10);
+        account.addInterest();
+        double balAfterInterest = 1000 * (10 + 1 / 100.0);
+        System.out.println("Actual balance: $" + account.getBalance());
+        System.out.println("Expected balance: 1100.0");
+    }
 }
